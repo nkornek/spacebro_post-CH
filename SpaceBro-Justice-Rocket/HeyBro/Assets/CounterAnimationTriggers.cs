f2105e70dcdd8674b87d04da1b992e3ace8b252e
Nick Kornek <nkornek@gmail.com>
2015-02-19 18:54:08 -0500

    Started blocking in animations

--- SpaceBro-Justice-Rocket/HeyBro/Assets/CounterAnimationTriggers.cs
+++ SpaceBro-Justice-Rocket/HeyBro/Assets/CounterAnimationTriggers.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class CounterAnimationTriggers : MonoBehaviour {
 
+	public ParticleSystem[] rocketParticles;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -11,5 +13,19 @@ public class CounterAnimationTriggers : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void ToggleRockets()
+	{
+		if (rocketParticles[0].enableEmission)
+		{
+			rocketParticles[0].enableEmission = false;
+			rocketParticles[1].enableEmission = false;
+		}
+		else
+		{
+			rocketParticles[0].enableEmission = true;
+			rocketParticles[1].enableEmission = true;
+		}
 	}
 }
